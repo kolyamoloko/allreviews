123d35cc1562018d2c9aa53c7d24c6f07caeb4f3
Mikalai <45792926+kolyamoloko@users.noreply.github.com>
2022-12-29 14:35:59 +0400

    client: user create

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import {Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
+
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-        Hello world.
-    </h1>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="login" element={<Login />} />
+      <Route path="login" element={<Login />} />
+      <Route path="login" element={<Login />} />
+    </Routes>
   )
 }
 
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import jwt_decode from 'jwt-decode';
+import { GoogleLogin } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import { client } from '../client';
+const Login = () => {
+  const navigate = useNavigate();
+  const onSuccess = (credentialResponse)=> {
+    let decoded = jwt_decode(credentialResponse.credential);
+    console.log(decoded)
+    localStorage.setItem('user',JSON.stringify(decoded))
+    const {name, aud, picture } = decoded;
+    const doc = {
+      _id: aud,
+      _type: 'user',
+      userName: name,
+      image: picture,
+    }
+    client.createIfNotExists(doc).then(()=> {
+      navigate('/', {replace: true })
+    })
+  }
+    
+  return (
+    <div className="flex justify-center items-center flex-col h-screen">
+      <h1>AllReviews</h1>
+      <GoogleLogin
+        onSuccess={onSuccess}
+        onError={() => {
+          console.log('Login Failed');
+          }}
+      />
+    </div>
+  );
+};
+
+export default Login;
